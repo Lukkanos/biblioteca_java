31dfacb8233888f8217b6c128580b88ea7c92b02
Lukkanos <lukkas_caetano@hotmail.com>
2024-11-03 22:30:12 -0300

    Create Main.java

---
+++ biblioteca_em_java/src/livro/Main.java
@@ -0,0 +1,14 @@
+package livro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Livraria livraria = new Livraria();
+		livraria.cadastroAutro();
+		livraria.cadastro();
+		livraria.listarLivro();
+	}
+
+}
